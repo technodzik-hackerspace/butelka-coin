d0f174c33270d1572ab5ff2697418e77a5c1e4cb
Yauhen Huchok <github@yauhen.io>
2024-08-04 00:43:39 +0200

    feat: add admin panel

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PrintController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,3 +9,7 @@ Route::get('/', function () {
 });
 
 Route::get('/print', [PrintController::class, 'printPage']);
+
+Auth::routes(['register' => false]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
